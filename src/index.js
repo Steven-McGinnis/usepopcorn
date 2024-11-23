ec6a6f81147cdf8c0edfb6dc4962a990b36e3062
Steven McGinnis <steven.mcginnis@itmmarketing.com>
2024-11-23 14:56:00 -0500

    Refactor CSS styles and update main application structure

--- src/index.js
+++ src/index.js
@@ -1,38 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-import StarRating from './StarRating';
+import './index.css';
+import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
-      maxRating={5}
-      messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
-    />
-    <StarRating
-      size={24}
-      maxRating={15}
-      color='red'
-      className='test'
-      defaultRating={3}
-    />
-    <Test />
+    <App />
   </React.StrictMode>
 );
-
-function Test() {
-  const [movieRating, setMovieRating] = React.useState(0);
-  return (
-    <div>
-      <StarRating
-        maxRating={'5'}
-        onSetRating={setMovieRating}
-        messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
-      />
-      <p>Movie rating: {movieRating}</p>
-    </div>
-  );
-}
